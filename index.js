e050653104c4fa25a81270ea915b4bef30c2a432
dinh0910 <dinhpoor0910@gmail.com>
2022-10-12 23:15:27 +0700

    vinh committed

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var session = require('express-session')
+
+var indexRouter = require('./routes/index')
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.use(session({
+    name: 'Demo Website Bootstrap 5',
+    secret: 'I dunt know',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 30 * 86400000
+    }
+}));
+
+app.use(function (req, res, next) {
+    res.locals.session = req.session;
+
+    var error = req.session.error;
+    var success = req.session.success;
+
+    delete req.session.error;
+    delete req.session.success;
+
+    res.locals.errorMsg = '';
+    res.locals.successMsg = '';
+
+    if (error) res.locals.errorMsg = error;
+    if (success) res.locals.successMsg = success;
+
+    next();
+});
+
+app.use('/', indexRouter)
+
+app.listen(3000, function () {
+    console.log('Server is running! :D')
+});
